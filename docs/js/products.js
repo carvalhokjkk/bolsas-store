e0aa34acf1656865479e901cd27a6002aa4943bc
User <carvalho2006elias@gmail.com>
2026-09-17 02:08:31 -0300

    update

---
+++ docs/js/products.js
@@ -0,0 +1,17 @@
+function add_event_listner() {
+    document.querySelectorAll('#catalogo .products-container .product-card').forEach(
+        (element) => {
+            element.addEventListener('click', () => open_page(element))
+        }
+    )
+}
+
+function open_page(element) {
+    window.location.href = '../docs/pages/produtct_page.html'
+}
+
+function back_menu(){
+    window.location.href = '../'
+}
+
+add_event_listner()
